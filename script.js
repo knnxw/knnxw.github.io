9c2d1d4a2ef08945b49dd28c6527ca621eeb2423
kenan <160174826+knnxw@users.noreply.github.com>
2024-03-15 23:31:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('yes-btn').addEventListener('click', function() {
+    window.location.href = 'evet.html'; // Bu dosyayı ayrıca oluşturmanız gerekir.
+});
+
+document.getElementById('no-btn').addEventListener('click', function() {
+    window.location.href = 'hayir.html'; // Bu dosyayı ayrıca oluşturmanız gerekir.
+});
+
